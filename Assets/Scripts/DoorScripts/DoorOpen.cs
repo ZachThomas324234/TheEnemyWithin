879b90c08b6ae7eba1fc1467d9ef9fd33099e17b
ZachThomas324234 <146828835+ZachThomas324234@users.noreply.github.com>
2024-03-28 15:26:40 +0000

    LEVEL DESIGN. dOOR

--- Assets/Scripts/DoorScripts/DoorOpen.cs
+++ Assets/Scripts/DoorScripts/DoorOpen.cs
@@ -5,25 +5,51 @@ using UnityEngine;
 public class DoorOpen : MonoBehaviour
 {
 
-    bool doorIsOpen;
+    public bool doorIsOpen;
+
+    public float Speed;
+
+    private Vector3 OriginPosition;
+    public Vector3 OpenPosition;
+    private Vector3 TargetPosition;
+
+    private PlayerMovement playerMovement;
+
 
     private void Start()
     {
+        OriginPosition = transform.position;
+        TargetPosition = OriginPosition;
+        playerMovement = FindAnyObjectByType<PlayerMovement>();
         Debug.Log("door is closed");
         doorIsOpen = false;
     }
 
     private void Update()
     {
-
+        transform.position = Vector3.Slerp(transform.position, TargetPosition, Speed);
+        if(Vector3.Distance(OriginPosition, playerMovement.transform.position) < 5)
+        {
+            if(!doorIsOpen)TheDoorOpens();
+        }
+        else
+        {
+            if(doorIsOpen)TheDoorCloses();
+        }
     }
 
-
+    private void TheDoorCloses()
+    {
+        Debug.Log("door is closed");
+        doorIsOpen = false;
+        TargetPosition = OriginPosition;
+    }
 
     private void TheDoorOpens()
     {
         Debug.Log("door is open");
         doorIsOpen = true;
+        TargetPosition += OpenPosition;
     }
 
 }
